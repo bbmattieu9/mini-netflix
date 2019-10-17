e62f6915560f882cf87e2b00bb8bc7d500c661dd
bbmattieu9 <bbmattieu9@gmail.com>
2019-10-17 15:38:07 +0100

    setting up project folder with assets for the project

---
+++ netflix/src/app/movies/movie-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app-movie-list',
+    template: `
+        <div>
+            <h1> List of Movies</h1>
+        </div>
+    `
+})
+export class MovieListComponent {
+
+}
